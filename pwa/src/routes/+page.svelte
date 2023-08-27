47e2069f768557842610924e9e69e696acdd8d8c
0xC9C3 <0xc9c3@0xc3.sh>
2023-08-27 18:47:54 +0200

    add username and password inputs to pwa

--- pwa/src/routes/+page.svelte
+++ pwa/src/routes/+page.svelte
@@ -1,32 +1,69 @@
 <script>
-    import {Button, Input, Spinner} from "flowbite-svelte";
+    import {Button, Input, Label, Spinner} from "flowbite-svelte";
     import {ble} from "$lib/ble";
     import TypeContentPacket from "$lib/packet/client/type_content";
+    import store from "$lib/store";
+    import {log} from "$lib/log";
 
     let isConnected = false;
     ble.currentConnection.subscribe(value => {
         isConnected = value?.device?.gatt?.connected ?? false;
     });
 
-    let text = "";
+    let username = "";
+    let password = "";
 
     let isSending = false;
-    const send = () => {
+
+    const sendText = async (text) => {
+        const packet = new TypeContentPacket(text).toBuffer();
+        await ble.send(packet);
+    }
+
+    const send = async () => {
         isSending = true;
-        const packet = new TypeContentPacket(
-            text
-        ).toBuffer();
-        ble.send(
-            packet
-        )
-            .finally(() => isSending = false);
+
+        const textsToType = [
+            username,
+            password,
+        ];
+        for (const text of textsToType) {
+            try {
+                if (text.length === 0) {
+                    continue;
+                }
+
+                await sendText(text);
+            } catch (e) {
+                store.toast.set({
+                    message: e.message,
+                    type: "error"
+                });
+
+                log.e(e);
+                break;
+            }
+        }
+
+        isSending = false;
     }
 </script>
 
 <div class="flex flex-col h-full">
 
     <form on:submit|preventDefault={() => send()}>
-        <Input bind:value={text} label="Name" on:submit={() => send()} placeholder="Enter something"/>
+        <div class="mb-2">
+            <Label class="mb-2" for="username">Username / E-Mail</Label>
+            <Input autocomplete="username" autocorrect="off" bind:value={username} id="username"
+                   placeholder="Username / E-Mail"
+                   type="text"/>
+        </div>
+        <div>
+            <Label class="mb-2" for="password">Password</Label>
+            <Input autocomplete="password" autocorrect="off" autofocus="autofocus" bind:value={password} id="password"
+                   placeholder="•••••••••"
+                   type="password"/>
+        </div>
     </form>
 
     <div class="flex-grow"/>
